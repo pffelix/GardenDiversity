1aefeb2260653d6b0ccf19007a96009e5ab0b734
Felix <felix.pfreundtner@gmail.com>
2022-02-22 20:42:27 +0100

    added UART code as module

--- src/firmware/asset_tracker_v2/src/main.c
+++ src/firmware/asset_tracker_v2/src/main.c
@@ -535,128 +535,11 @@ static void on_all_events(struct app_msg_data *msg)
 	}
 }
 
-/*
-* UART communication between nrf52840 and nrf9160
-*/
 
-#include <sys/printk.h>
-#include <drivers/uart.h>
-#include <string.h>
-#include <inttypes.h>
-
-#define UART_START_BYTE 0
-#define UART_START_BYTE_REPEAT 3
-#define UART_BUF_BYTES 1024
-#define UART_BUF_BYTES_16 (UART_BUF_BYTES / 2)
-static uint8_t uart_buf[UART_BUF_BYTES]; // same byte size as uart_buf_16
-static int16_t uart_buf_16[UART_BUF_BYTES_16]; // same byte size as uart_buf
-static int uart_start_byte_repeat = 0;
-static int uart_k = 0;
-static bool uart_low = true;
-static struct device *uart_dev;
-struct uart_config uart_cfg;
-int uart_ret;
-
-int send_data(const uint8_t *buf, size_t size)
-{
-	if (size == 0) {
-                return 0;
-	}
-	for(int i = 0; i < size; i++){
-                uart_poll_out(uart_dev, buf[i]);
-	}
-	return 0;
-}
-
-void uart_cb(struct device *x)
-{
-	uart_irq_update(x);
-	int data_length = 0;
-
-	if (uart_irq_rx_ready(x)) {
-                data_length = uart_fifo_read(x, uart_buf, sizeof(uart_buf));
-                uart_buf[data_length] = 0;
-	}
-	printk("%s", uart_buf);
-}
-
-int send_data_int16(int16_t *buf, size_t size)
-{
-	if (size == 0) {
-		return 0;
-	}
-
-        // send start byte
-        for(int i = 0; i < UART_START_BYTE_REPEAT; i++){
-                uart_poll_out(uart_dev, UART_START_BYTE);
-        }
-
-        // send bytes
-        uint8_t uart_u8[sizeof(uint16_t)];
-        uint16_t uart_u16;
-	for(int i = 0; i < size; i++){
-                uart_u16 = (uint16_t)(buf[i]);
-                uart_u8[0] =(uart_u16) & 0xFF;
-                uart_u8[1] = (uart_u16 >> 8) & 0xFF;
-                //check conversion
-                //int16_t uart16;
-                //int k2;
-                //for(int k = 0; k < 1; k++){
-                //  k2 = k * 2;
-                //  uart_u16 = (uart_u8[k2] | uart_u8[k2+1] << 8);
-                //  uart16 = (int16_t) uart_u16;
-                //  uart_buf_16[k] = uart16;
-                //  printk("%d \n", uart_buf_16[k]);
-                //}
-                for(int j = 0; j < sizeof(int16_t); j++){
-                        uart_poll_out(uart_dev, uart_u8[j]);
-                }
-	}
-	return 0;
-}
-
-void uart_cb_int16(struct device *x)
-{
-        uart_irq_update(x);
-        int data_length = 0;
-
-        if (uart_irq_rx_ready(x)) {
-          data_length = uart_fifo_read(x, uart_buf, sizeof(uart_buf));
-        }
-
-        uint16_t uart_u16;
-        int16_t uart16;
-        uint8_t uart_u8;
-        for(int k = 0; k < data_length; k++){
-                if(uart_buf[k] == UART_START_BYTE){ // update start byte
-                        uart_start_byte_repeat += 1;
-
-                }else{
-                        uart_start_byte_repeat = 0;
-                }
-
-                if(uart_start_byte_repeat == UART_START_BYTE_REPEAT){  // set uart buffer to start
-                        uart_k = 0;
-                        uart_low = true;
-                        continue;
-                }
-                if(uart_low){ // update lower byte
-                        uart_buf_16[uart_k] = (int16_t) uart_buf[k];
-                        uart_low = false;
-                }else{ // update higher byte
-                        uart_u8 = (uint8_t)uart_buf_16[uart_k];
-                        uart_u16 = uart_u8 | uart_buf[k] << 8;
-                        uart16 = (int16_t) uart_u16;
-                        uart_buf_16[uart_k] = uart16;
-                        printk("%d \n", uart_buf_16[uart_k]);
-                        uart_low = true;
-                        if(uart_k < UART_BUF_BYTES_16){ // if start byte missed: wait
-                                uart_k += 1; // else, increase uart buffer position
-                        }
-                }
-
-        }
-}
+ /*
+ * UART communication between nrf52840 and nrf9160
+ */
+#include "module_uart.h"
 
 
 void main(void)
@@ -665,27 +548,13 @@ void main(void)
         /*
         * UART communication between nrf52840 and nrf9160
         */
-	
-	uart_dev = device_get_binding("UART_1");
-        if (!uart_dev) {
-                printk("Could not get UART\n");
-	}
 
-        uart_ret = uart_config_get(uart_dev, &uart_cfg);
-        uart_cfg.baudrate = 1000000;
-        uart_cfg.parity = UART_CFG_PARITY_NONE;
-        uart_cfg.stop_bits = UART_CFG_STOP_BITS_1;
-        uart_cfg.data_bits = UART_CFG_DATA_BITS_8;
-        uart_cfg.flow_ctrl = UART_CFG_FLOW_CTRL_RTS_CTS;
-        uart_ret = uart_configure(uart_dev, &uart_cfg);
-	uart_irq_callback_set(uart_dev, uart_cb_int16);
-	uart_irq_rx_enable(uart_dev);
-	printk("UART 9160 start!\n");
+	uart_init();
         char hey[] = "hey from 9160 \n";
         uint16_t hey_int16[] = {-9160, 9160, -1000, 1000, -32768, 32767};
         while(1){
                 //send_data(hey, sizeof(hey));
-                send_data_int16(hey_int16, sizeof(hey_int16) / sizeof(int16_t));
+                uart_send_data_int16(hey_int16, sizeof(hey_int16) / sizeof(int16_t));
                 k_sleep(K_MSEC(1000));
         }
 
