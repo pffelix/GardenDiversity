bfb64c654b2dd9d5477ace0958570f0291b7ce2c
Felix <felix.pfreundtner@gmail.com>
2024-05-11 17:46:55 +0200

    updated installer

--- src/firmware/installer.sh
+++ src/firmware/installer.sh
@@ -1,8 +1,11 @@
 # install Python
-sudo apt install software-properties-common -y
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
-sudo apt install python3.9 -y
+# sudo apt install software-properties-common -y
+# sudo apt install python3-launchpadlib -y
+# sudo add-apt-repository ppa:deadsnakes/ppa -y
+# sudo apt update
+# sudo apt install python3.9 -y
+
+# Install PIP
 sudo apt install python3-pip -y
 
 # install I2S microphone driver
