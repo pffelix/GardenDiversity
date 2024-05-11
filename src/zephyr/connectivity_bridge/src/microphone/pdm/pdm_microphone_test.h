405e3593f9834bb72feccc9463673e126e6f65c8
Felix <felix.pfreundtner@gmail.com>
2024-05-11 10:43:14 +0200

    removed zephyr code

--- src/zephyr/connectivity_bridge/src/microphone/pdm/pdm_microphone_test.h
+++
@@ -1,7 +0,0 @@
-#ifndef K_PDM_MICROPHONE_TEST_H
-#define K_PDM_MICROPHONE_TEST_H
-
-
-void test_microphone(void);
-
-#endif
--- src/zephyr/connectivity_bridge/src/microphone/spi/spi_microphone.h
+++
@@ -1,7 +0,0 @@
-#ifndef K_SPI_MICROPHONE_H
-#define K_SPI_MICROPHONE_H
-
-
-void spi_test();
-
-#endif
